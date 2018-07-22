f9d8beebe9fa7d98fba4d1bcf774c399c9326ca9
KasparIsSo <kasparso421@gmail.com>
2018-07-22 13:26:34 -0400

    added prettier

---
+++ js/ClientApp.jsx
@@ -0,0 +1,24 @@
+const ce = React.createElement;
+
+const MyTitle = function(props) {
+  return ce(
+    "div",
+    null,
+    ce("h1", { style: { color: props.color } }, props.title)
+  );
+};
+
+const MyFirstComponent = function() {
+  return ce(
+    "div",
+    { id: "my-first-component" },
+    ce(MyTitle, { title: "Rick and Morty", color: "Red" }),
+    ce(MyTitle, { title: "Rick and Morty", color: "Teal" }),
+    ce(MyTitle, { title: "Rick and Morty", color: "Lime" }),
+    ce(MyTitle, { title: "Rick and Morty", color: "Peru" }),
+    [ce(MyTitle), ce(MyTitle), ce(MyTitle)],
+    ce("h1", null, "My first component!")
+  );
+};
+
+ReactDOM.render(ce(MyFirstComponent), document.getElementById("app"));
---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "printWidth": 120,
+  "tabWidth": 2,
+  "singleQuote": true,
+  "trailingComma": "es5",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "parser": "flow",
+  "semi": true,
+  "tabs": false
+  "[javascript]": {
+    "editor.formatOnSave": true
+  }
+}
